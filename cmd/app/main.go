4d322e6296f038b2d584264c54ef1f0ba1a4cf5d
vasiliyantufev <vasiliyantufev@gmail.com>
2022-07-19 17:22:30 +0300

    http

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"html/template"
 	"log"
@@ -11,7 +12,6 @@ import (
 const portNumber = ":8060"
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	//w.Write([]byte("We're live!!!!!"))
 	tmpl, _ := template.ParseFiles("./web/templates/index.html")
 	err := tmpl.Execute(w, "no data needed")
 	if err != nil {
@@ -19,13 +19,34 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//func test_page(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprint(w, "Test Page")
+//}
+//func handler(w http.ResponseWriter, r *http.Request, mystr string) {
+//	println(mystr)
+//}
+
+func OrderHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Order: %v\n", vars["id"])
+}
+
 func handleRequest() {
-	http.HandleFunc("/", home_page)
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", home_page)
+
+	//hi := "hello"
+	//rtr.HandleFunc("/order", func(w http.ResponseWriter, r *http.Request) {
+	//	handler(w, r, hi)
+	//})
+
+	rtr.HandleFunc("/order/{id}/", OrderHandler)
+
 	fmt.Printf("Starting application on port %v\n", portNumber)
-	http.ListenAndServe(portNumber, nil)
+	http.ListenAndServe(portNumber, rtr)
 }
 
 func main() {
 	handleRequest()
-
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/vasiliyantufev/wb-l0
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.8.1 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jackc/pgx/v4 v4.16.1
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/lib/pq v1.10.6 // indirect
 	github.com/nats-io/stan.go v0.10.2
 )
